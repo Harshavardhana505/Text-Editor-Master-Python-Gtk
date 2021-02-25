89443c87869132ca38005853437592893ce0fbc6
Harshavardhana <75427218+Harshavardhana505@users.noreply.github.com>
2021-02-25 15:35:29 +0530

    Update text-editor.py

--- Text-Editor-master/text-editor.py
+++ Text-Editor-master/text-editor.py
@@ -1,28 +1,7 @@
 #!/usr/bin/python3
 # -*- coding:utf8 -*-
 """
-Copyright (C) 2012 Skyler Riske
-
-This program is licensed under the GNU GPLv3, see LICENSE for details.
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-Author(s): Skyler Riske
-
-umte.py
-
-umte, or "Uber Minimal Text Editor" is a python Gtk3 text editor built
-with simplicity in mind.
+d
 """
 
 import os
